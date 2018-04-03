9530e7ba0915e205fbffa27e735577cb24be7e1a
ismael1987 <ismael1987@example.com>
2018-04-03 19:59:17 +0200

    mongo db

---
+++ src/main/java/com/Mp9MyPersonalToDoList/Mp9MyPersonalToDoList/Mp9MyPersonalToDoListApplication.java
@@ -0,0 +1,12 @@
+package com.Mp9MyPersonalToDoList.Mp9MyPersonalToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp9MyPersonalToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp9MyPersonalToDoListApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Mp9MyPersonalToDoList/Mp9MyPersonalToDoList/Mp9MyPersonalToDoListApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Mp9MyPersonalToDoList.Mp9MyPersonalToDoList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp9MyPersonalToDoListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
